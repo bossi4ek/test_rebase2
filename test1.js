dc7df8677c2251d7e5567cbb14871e6541cdecf0
Sergey Levchenko <bossi4ek@gmail.com>
2016-08-30 10:56:02 +0300

    change code in br1 for second rebase

--- test1.js
+++ test1.js
@@ -2,5 +2,5 @@
  * Created by BOSS-WORK on 30.08.2016.
  */
 //test1
-//br1 ... change1
+//br1 ... change1 ... change2
 //br2
--- test3.js
+++ test3.js
@@ -1,6 +1,4 @@
 /**
  * Created by BOSS-WORK on 30.08.2016.
  */
-//test2
-//br1 ... change1 ... change2
-//br2
+//test3
